724e74a674e485c46f77deff3cae8ea3eead28fe
a1111 <klimko.kirya@mail.ru>
2023-02-03 20:54:08 +0300

    asd

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ bot = Bot(token="6052931568:AAE_6CjKCrpS7LNna-HIN4KhG3ix2eCIBHU")
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
+async def echo(message: types.Message):
+    await message.reply(message.text)
+
+@dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!")
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+    await message.reply("Напиши мнибудь, и я отпрпавлю этот текст тебе в ответ!")
 
 
 if __name__ == '__main__':
